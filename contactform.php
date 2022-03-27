3838c96565d3ba57f68801a7fb65e32c0da68d7d
mstoyanov7 <92221668+mstoyanov7@users.noreply.github.com>
2022-03-27 17:09:44 +0300

    update contactform

--- contactform.php
+++ contactform.php
@@ -1,14 +1,15 @@
 <?php
-if (isset(&_POST['submit'])) {
-    &name = &_POST['name'];
-    &email = &_POST['email'];
-    &subject = &_POST['subject'];
-    &message = &_POST['message'];
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-    &recipient = "cyberpc@abv.bg";
-    &mailheader = "From: ".&email;
-    &txt = "Вие получихте съобщение от ".&name.".\n\n".&message;
+    $recipient = "cyberpc@abv.bg";
+    $mailheader = "From: ".$email;
+    $txt = "Вие получихте съобщение от ".$name.".\n\n".$message;
 
-    mail(&recipient, &subject, $txt, header);
+    mail($recipient, $subject, $txt, $mailheader);
+    header("Location: index.html");
     }
 ?>
