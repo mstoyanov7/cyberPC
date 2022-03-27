06d5e5a19fffda7fda97ebe087ee4ea4106b47aa
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2022-03-27 19:57:20 +0300

    Adding the mail functionality

---
+++ js/sendEmail.js
@@ -0,0 +1,20 @@
+const form = Array.from(document.querySelector("#form-email"));
+const btn = document.querySelector("#submit-btn");
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let data = form.map((e) => {
+    return e.value;
+  });
+  let tempParams = {
+    name: data[0],
+    subject: data[2],
+    message: data[3],
+  };
+  sendEmail(tempParams);
+});
+
+function sendEmail(tempParams) {
+  emailjs.send( "template_kcvnpp9", tempParams).then((res) => {
+    console.log("success", res.status);
+  });
+}
